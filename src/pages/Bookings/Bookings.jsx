84b16b5ce54c9419a80eabb0215926f435440fdd
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-31 00:35:31 +0600

    M(61-4) withCrediential true for passing  email as cookie

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -12,10 +12,10 @@ const Bookings = () => {
 
     useEffect(() => {
 
-        axios.get(url)
-        .then(res=>{
-            setBookings(res.data);
-        })
+        axios.get(url, { withCredentials: true })
+            .then(res => {
+                setBookings(res.data);
+            })
 
         // fetch(url)
         //     .then(res => res.json())
@@ -34,33 +34,33 @@ const Bookings = () => {
                     console.log(data);
                     if (data.deletedCount > 0) {
                         alert('Deleted Successful')
-                        const remaining = bookings.filter(booking=>booking._id !== id);
+                        const remaining = bookings.filter(booking => booking._id !== id);
                         setBookings(remaining);
                     }
                 })
         }
     }
 
-    const handleBookingConfirm =(id)=>{
-        fetch(`http://localhost:5000/bookings/${id}`,{
-            method:'PATCH',
-            headers:{
-                'content-type':'application/json'
+    const handleBookingConfirm = (id) => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
             },
-            body:JSON.stringify({status:'confirm'})
+            body: JSON.stringify({ status: 'confirm' })
         })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            if(data.modifiedCount > 0){
-                // update state
-                const remaining = bookings.filter(booking=>booking._id !== id);
-                const updated = bookings.find(booking=>booking._id === id);
-                updated.status = 'confirm';
-                const newBookings = [updated, ...remaining];
-                setBookings(newBookings);
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // update state
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id);
+                    updated.status = 'confirm';
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                }
+            })
     }
 
     return (
@@ -90,7 +90,7 @@ const Bookings = () => {
                                 booking={booking}
                                 handleDelete={handleDelete}
                                 handleBookingConfirm={handleBookingConfirm}
-                                >
+                            >
                             </BookingRow>)
                         }
                     </tbody>
